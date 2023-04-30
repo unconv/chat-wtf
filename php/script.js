b7ac962ad7c177c6faa2ac1a691a682883750f04
unconv <unconventionalcoding@gmail.com>
2023-04-30 05:55:53 +0300

    improved markdown to html conversion

--- php/script.js
+++ php/script.js
@@ -69,15 +69,8 @@ function add_message( direction, message ) {
 }
 
 function update_message( message, new_message ) {
-    // add ending code block tags when missing
-    let code_block_count = (new_message.match(/```/g) || []).length;
-    if( code_block_count % 2 !== 0 ) {
-        new_message += "\n```";
-    }
-
-    // convert code blocks into markdown
-    // @todo: convert single ticks as well
-    new_message = format_code_blocks( new_message );
+    // convert message from Markdown to HTML
+    new_message = convert_markdown( new_message );
 
     // update message content
     message.innerHTML = '<p>' + new_message + "</p>";
@@ -87,20 +80,47 @@ function update_message( message, new_message ) {
 }
 
 /**
- * Converts code blocks in Markdown into HTML
+ * Converts Markdown formatted response into HTML
  *
  * @param {string} text Markdown formatted text
  * @returns HTML formatted text
  */
-function format_code_blocks( text ) {
-    let formatted_message = "";
+function convert_markdown( text ) {
+    // add ending code block tags when missing
+    let code_block_count = (text.match(/```/g) || []).length;
+    if( code_block_count % 2 !== 0 ) {
+        text += "\n```";
+    }
 
-    let code_blocks = text.split("```");
+    // HTML-escape parts of text that are not inside ticks.
+    // This prevents <?php from turning into a comment tag
+    let escaped_parts = [];
+    let code_parts = text.split("`");
+    for( let i = 0; i < code_parts.length; i++ ) {
+        if( i % 2 === 0 ) {
+            escaped_parts.push( escapeHtml( code_parts[i] ) );
+        } else {
+            escaped_parts.push( code_parts[i] );
+        }
+    }
+    let escaped_message = escaped_parts.join("`");
+
+    // Convert Markdown to HTML
+    let formatted_message = "";
+    let code_blocks = escaped_message.split("```");
     for( let i = 0; i < code_blocks.length; i++ ) {
         if( i % 2 === 0 ) {
-            formatted_message += escapeHtml( code_blocks[i].trim() ).replace( /\n/g, "<br>" );
+            // add two spaces in the end of every line
+            // for non-codeblocks so that one-per-line lists
+            // without markdown can be generated
+            formatted_message += markdown_converter.makeHtml(
+                code_blocks[i].trim().replace( /\n/g, "  \n" )
+            );
         } else {
-            formatted_message += markdown_converter.makeHtml( "```" + code_blocks[i] + "```" );
+            // convert Markdown code blocks to HTML
+            formatted_message += markdown_converter.makeHtml(
+                "```" + code_blocks[i] + "```"
+            );
         }
     }
 
