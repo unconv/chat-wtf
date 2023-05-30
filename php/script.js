74d3b2cc981fcfe728d1059c31087e22450a6daf
unconv <unconventionalcoding@gmail.com>
2023-05-30 14:24:48 +0300

    refactor and document

--- php/script.js
+++ php/script.js
@@ -6,6 +6,7 @@ const markdown_converter = new showdown.Converter({
     requireSpaceBeforeHeadingText: true
 });
 
+// detect Enter on message input to send message
 message_input.addEventListener( "keydown", function( e ) {
     if( e.keyCode == 13 && !e.shiftKey ) {
         e.preventDefault();
@@ -15,7 +16,17 @@ message_input.addEventListener( "keydown", function( e ) {
     }
 } );
 
-function create_title( question, answer, title_box, chat_id ) {
+/**
+ * Creates a title for a conversation
+ * based on user's question and
+ * ChatGPT's answer
+ * 
+ * @param {string} question User's question
+ * @param {string} answer ChatGPT's answer
+ * @param {HTMLElement} title_link Title link element
+ * @param {string} chat_id ID of the conversation
+ */
+function create_title( question, answer, title_link, chat_id ) {
     const data = new FormData();
     data.append('question', question);
     data.append('answer', answer);
@@ -27,14 +38,18 @@ function create_title( question, answer, title_box, chat_id ) {
     })
     .then(response => response.text())
     .then(responseText => {
-        title_box.textContent = responseText;
-        title_box.setAttribute( "title", responseText );
+        title_link.textContent = responseText;
+        title_link.setAttribute( "title", responseText );
     })
     .catch(error => {
         console.log(error);
     });
 }
 
+/**
+ * Sends a message to ChatGPT and appends the
+ * message and the response to the chat
+ */
 function send_message() {
     let question = message_input.value;
 
@@ -78,16 +93,7 @@ function send_message() {
         message_list.scrollTop = message_list.scrollHeight;
 
         if( new_chat ) {
-            let title_box = document.createElement( "li" );
-            let title_link = document.createElement( "a" );
-            title_link.setAttribute( "href", base_uri + "index.php?chat_id=" + chat_id );
-            title_box.appendChild( title_link );
-            let delete_button = document.createElement( "button" );
-            delete_button.setAttribute( "data-id", chat_id );
-            delete_button.classList.add( "delete" );
-            delete_button.textContent = "X";
-            title_box.appendChild( delete_button );
-            new_chat_link.appendChild( title_box );
+            let title_link = create_chat_link( chat_id );
 
             create_title( question, response, title_link, chat_id );
         }
@@ -96,6 +102,38 @@ function send_message() {
     message_input.focus();
 }
 
+/**
+ * Creates a new chat conversation link
+ * to the sidebar
+ *
+ * @returns Title link element
+ */
+function create_chat_link() {
+    let link = base_uri + "index.php?chat_id=" + chat_id;
+    let title_link = document.createElement( "a" );
+        title_link.setAttribute( "href", link );
+
+    let delete_button = document.createElement( "button" );
+        delete_button.setAttribute( "data-id", chat_id );
+        delete_button.classList.add( "delete" );
+        delete_button.textContent = "X";
+
+    let title_box = document.createElement( "li" );
+        title_box.appendChild( title_link );
+        title_box.appendChild( delete_button );
+
+    new_chat_link.appendChild( title_box );
+
+    return title_link;
+}
+
+/**
+ * Adds a message to the message list
+ *
+ * @param {string} direction incoming/outgoing
+ * @param {string} message The message to add
+ * @returns The added message element
+ */
 function add_message( direction, message ) {
     const message_item = document.createElement( "div" );
     message_item.classList.add( "chat-message" );
@@ -107,6 +145,12 @@ function add_message( direction, message ) {
     return message_item;
 }
 
+/**
+ * Updates a chat message with the given text
+ *
+ * @param {HTMLElement} message The message element to update
+ * @param {string} new_message The new message
+ */
 function update_message( message, new_message ) {
     // convert message from Markdown to HTML
     new_message = convert_markdown( new_message );
@@ -185,27 +229,39 @@ function escapeHtml( text ) {
     return text.replace(/[&<>"']/g, function(m) { return map[m]; });
 }
 
+// events to run when page loads
 document.addEventListener( "DOMContentLoaded", function() {
+    // markdown format all messages in view
     let messages = document.querySelectorAll( ".chat-message" );
-
     messages.forEach( function( message ) {
         update_message( message, message.textContent );
     } );
 
+    // event listeners
     document.body.addEventListener( "click", function(e) {
         if( e.target.matches( "button.delete" ) ) {
-            if( ! confirm( "Are you sure you want to delete this conversation?" ) ) {
-                return false;
-            }
-
-            const chat_id = e.target.getAttribute( "data-id" );
-            const data = new FormData();
-            data.append( "chat_id", chat_id );
-            fetch( base_uri + "delete_chat.php", {
-                method: 'POST',
-                body: data
-            } );
-            e.target.parentNode.remove();
+            delete_button_action( e );
         }
     } );
 } );
+
+/**
+ * Handles the chat conversation delete button click
+ *
+ * @param {Event} e The delete button click event
+ */
+function delete_button_action( e ) {
+    if( ! confirm( "Are you sure you want to delete this conversation?" ) ) {
+        return;
+    }
+
+    const chat_id = e.target.getAttribute( "data-id" );
+    const data = new FormData();
+    data.append( "chat_id", chat_id );
+    // @todo: add error handling
+    fetch( base_uri + "delete_chat.php", {
+        method: 'POST',
+        body: data
+    } );
+    e.target.parentNode.remove();
+}
